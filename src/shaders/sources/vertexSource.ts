73f39f9025b34c441e7e25fc6f1116fd9e2907ed
unknown <bjinsu93@gmail.com>
2022-06-07 02:27:46 +0900

    feat: vertex source transform/viewProjection matrix 추가

--- src/shaders/sources/vertexSource.ts
+++ src/shaders/sources/vertexSource.ts
@@ -2,13 +2,16 @@ export const veretxSource = `
 attribute vec3 aPosition;
 attribute vec3 aColor;
 
+uniform mat4 transformMatrix;
+uniform mat4 viewProjectionMatrix;
+
 varying vec3 vColor;
 
 void main() {
 
-   gl_Position = vec4(aPosition, 1.0);
-   // gl_Position = vec4(aPosition * 0.01, 1.0);
-    //gl_Position = vec4(0.5,0.5,0.5,1.0);
+    vec4 position = viewProjectionMatrix * transformMatrix * vec4(aPosition, 1.0);
+    // position.x += sin(10.);
+    gl_Position = position;
     gl_PointSize = 2.;
 
     vColor = aColor;
