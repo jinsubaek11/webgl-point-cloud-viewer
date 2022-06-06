cc0d27c16610da8bcbe344a42e615e7a39ca77c4
unknown <bjinsu93@gmail.com>
2022-06-07 02:29:52 +0900

    feat: mockdata 범위 수정

--- src/utilities/util.ts
+++ src/utilities/util.ts
@@ -1,7 +1,13 @@
 export function getData() {
+	// const data = Array(10000).fill(0).map(v => [
+	// 	(Math.random()-.5) * 1.5, (Math.random()-.5) * 1.5, (Math.random()-.5) * 1.5, 0, Math.random() * 255, Math.random() * 255,Math.random() * 255,
+	// ]).flat()
+
 	const data = Array(10000).fill(0).map(v => [
-		(Math.random()-.5) * 2, (Math.random()-.5) * 2, (Math.random()-.5) * 2, 0, Math.random() * 255, Math.random() * 255,Math.random() * 255,
+		(Math.random()-.5)*300, (Math.random()-.5)*300, (Math.random()) * 300, 0, Math.random() * 255, Math.random() * 255,Math.random() * 255,
 	]).flat()
+
+	// console.log(data)
 
 	return data
 }
